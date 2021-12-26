c3474ad73b6003ce18816326d1bfc9e7a6743777
Finn St John <stjohn.finn@gmail.com>
2021-12-25 22:08:47 -0800

    figured out responsive menu kinda

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(document).ready( function() {
     
     $('#text-inp').focusout(textInpFocusout);
+    $(window).resize(checkHeaderWidth);
+    $('#rotate-arrow').click(rotateArrow);
 
 });
 
@@ -16,4 +18,34 @@ function textInpFocusout() {
     }
 
     console.log('focusout!');
+}
+
+function checkHeaderWidth() {
+    const viewportWidth = window.innerWidth;
+
+    if (viewportWidth < 850) {
+        $('#menu').html('<div><div id=three-bar-menu></div></div>')
+        $('#menu')
+            .css('width', 'min-content')
+            .css('height', '20px')
+            .css('padding-right', '1rem');
+    } else if (viewportWidth > 850) {
+        $('#menu').html('<p>About</p><p>Contact</p><p id="signin">Sign In</p>')
+        $('#menu').css('width', '90%');
+    }
+}
+
+function rotateArrow() {
+
+    if ($('#rotate-arrow').hasClass('arrow-active')) {
+        $('#rotate-arrow').removeClass('arrow-active');
+        $('#rotate-arrow').addClass('arrow-inactive');
+        $('#arrow-menu').removeClass('menu-visible');
+        $('#arrow-menu').addClass('menu-invisible');
+    } else {
+        $('#rotate-arrow').removeClass('arrow-inactive');
+        $('#rotate-arrow').addClass('arrow-active');
+        $('#arrow-menu').removeClass('menu-invisible');
+        $('#arrow-menu').addClass('menu-visible');
+    }
 }
